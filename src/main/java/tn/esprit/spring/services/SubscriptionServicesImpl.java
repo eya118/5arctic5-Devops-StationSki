c7cb176dcc71584912bb10c0aaae25d39d45bae9
Molka.Kbaier <molka.kbaier@esprit.tn>
2024-11-03 15:39:43 +0100

    modif

--- src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
@@ -70,22 +70,23 @@ public class SubscriptionServicesImpl implements ISubscriptionServices{
     }
 
    // @Scheduled(cron = "* 0 9 1 * *") /* Cron expression to run a job every month at 9am */
-   @Scheduled(cron = "*/30 * * * * *") /* Cron expression to run a job every 30 seconds */
+   @Scheduled(cron = "*/30 * * * * *") // Exécute toutes les 30 secondes
    public void showMonthlyRecurringRevenue() {
        Float monthlyRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.MONTHLY);
        Float semestrielRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.SEMESTRIEL);
        Float annualRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.ANNUAL);
 
-       // Set revenue to 0 if it is null
+       // Vérifiez si les revenus sont nuls et remplacez-les par zéro
        if (monthlyRevenue == null) monthlyRevenue = 0f;
        if (semestrielRevenue == null) semestrielRevenue = 0f;
        if (annualRevenue == null) annualRevenue = 0f;
 
-       // Calculate monthly revenue
+       // Calculez les revenus mensuels
        Float revenue = monthlyRevenue +
                (semestrielRevenue / 6) +
                (annualRevenue / 12);
 
        log.info("Monthly Revenue = " + revenue);
    }
+
 }
